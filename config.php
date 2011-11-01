6c73f6fe449c7b76abcdae0881c9c647d3c20dca
Nicolas Kayser-Bril <n.kayserbril@gmail.com>
2011-11-01 11:34:55 +0100

    Added password for prod environment

--- config.php
+++ config.php
@@ -14,23 +14,36 @@ require_once "libraries/alpha.id.php";
 //Function to transpose arrays
 require_once "libraries/transpose.php";
 
-//Connects to the local DB
-$hostname = "localhost";
-$database = "datastory";
-$username = "root";
-$password = "";
+$prod_domain = "windowonthemedia.com";
 
-$mysqli = new mysqli($hostname,$username,$password,$database);
+if (strstr($_SERVER['SERVER_NAME'], $prod_domain)){		
 
-$debug = 1;
+	// PROD ENVIRONMENT //
 
-if ($debug == 1){
+	$user_id = "0";
+
+	//Global const
+	define("BASE_DIR", "http://windowonthemedia.com/datastory/");
+
+	//Fetches passwords
+	require_once('passwords.php');
+	
+
+}else{
+
+	// DEV ENVIRONMENT //
+
 	$user_id = "0";
 	error_reporting(E_ALL);
-	define("BASE_DIR",     "http://localhost/Data-Story/");
-}else{
-	//Global const
-	define("BASE_DIR",     "http://datastory.de/");
+	define("BASE_DIR", "http://localhost/Data-Story/");
+
+	//Connects to the local DB
+	$hostname = "localhost";
+	$database = "datastory";
+	$username = "root";
+	$password = "";
 }
+
+$mysqli = new mysqli($hostname,$username,$password,$database);
 
 ?>
